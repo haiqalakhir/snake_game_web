f6c8491f82256cefa14a11ad9e3deaa92163f34b
oppanjang <mohamadhaiqalakhir@yahoo.com>
2023-12-23 22:40:33 +0800

    silap grid copy paste

--- scrip.js
+++ scrip.js
@@ -93,12 +93,12 @@ document.addEventListener('keydown', function(e) {
         snake.dx = 0;
     }
     else if (e.which === 39 && snake.dy === 0) {
-        snake.dy = -grid;
-        snake.dx = 0;
+        snake.dx = grid;
+        snake.dy = 0;
     }
 
     else if (e.which === 40 && snake.dy === 0) {
-        snake.dy = -grid;
+        snake.dy = grid;
         snake.dx = 0;
     }
 })
